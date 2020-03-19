ce90dce41a8eb5145a32dde05706b099b8469f37
manny i. agbayani <serverless.yoda@gmail.com>
2020-03-19 20:07:27 +1300

    add CourseController

---
+++ Clean.Architecture/CA.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CA.Application.Interfaces;
+using CA.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CA.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService service;
+
+        public CourseController(ICourseService service)
+        {
+            this.service = service;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = service.GetCourses();
+            return View(model);
+        }
+    }
+}
